1971e0dcb4e36b7c7bf8b1ba5362fba075da73ed
Sunshinetsu0411 <yewhang2004@gmail.com>
2024-07-23 18:17:43 +0800

    add main.py

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from discord.ext import commands
 
 import botToken # for discord's bot token
 
-class homelessBeatingBot(commands.bot):
+class homelessBeatingBot(commands.Bot):
     async def on_ready(self):
         print("It is homeless beating time everyone!!!!")
 
 intents = discord.Intents.all()
 
-bot = hoemlessBeatingBot(command_prefix = ".hb", intents = intents)
+bot = homelessBeatingBot(command_prefix = ".hb", intents = intents, activity = discord.Activity(name = "homeless beating", type = discord.ActivityType.competing, state = "In competition... Currently rank #1", timestamps = {"start":1721698794000}))
+# discord.Activity() class with attributes:
+# `type` - set up the activity type for the bot, in this case, discord.ActivityType.competing
+# `state` - kinda like a caption for the activity type, take a string and display it when looking to the bot profile
+# `timestamps` - a dictionary with two keys: `start` & `end`, set up the start and end time of the activity in Unix epoch, milliseconds (doesn't display it though, but can be retrieve later in property)
 bot.run(botToken.botToken)
