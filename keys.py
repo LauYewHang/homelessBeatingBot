71ae2ebf0b6aaad6b93477d46e7bb490e2b7ad4f
sunshinetsu0411 <yewhang2004@gmail.com>
2024-07-26 00:21:58 +0800

    add variable db_name to keys.py

---
+++ keys.py
@@ -0,0 +1,8 @@
+# disord bot token
+botToken = ".your discord bot token"
+
+# database information
+db_host = "your database host"
+db_user = "your database user name"
+db_user_password = "your database user password"
+db_name = "your database name"
