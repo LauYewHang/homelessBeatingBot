d6816163fccc2779d4807f55745c261000fb69bb
sunshinetsu0411 <yewhang2004@gmail.com>
2024-07-25 23:44:19 +0800

    change botToken.py to keys.py

--- keys.py
+++
@@ -1,7 +0,0 @@
-# disord bot token
-botToken = "your bot token here"
-
-# database information
-db_host = "your database host name"
-db_user = "your database user name"
-db_user_password = "your database user password"
