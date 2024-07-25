e1bdf83cfcc531aeb25588e79d17016950fab7bc
sunshinetsu0411 <yewhang2004@gmail.com>
2024-07-25 23:03:51 +0800

    change botToken.py to keys.py so it can save databse information too (like username and password)

---
+++ keys.py
@@ -0,0 +1,7 @@
+# disord bot token
+botToken = "your bot token here"
+
+# database information
+db_host = "your database host name"
+db_user = "your database user name"
+db_user_password = "your database user password"
