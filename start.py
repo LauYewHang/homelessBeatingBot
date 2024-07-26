23a3dcdf6e8de284857a31847c0ce51275d8edac
sunshinetsu0411 <yewhang2004@gmail.com>
2024-07-27 04:04:23 +0800

    add start command to register player to database

--- start.py
+++ start.py
@@ -1,12 +1,59 @@
 import discord
 from discord.ext import commands
 import mysql.connector
+import random
+import os
 
 from keys import *
+
+memeQuantity = len(os.listdir("homelessMeme/"))
 
 # connect to the database
 db = mysql.connector.connect(
     host = db_host,
     user = db_user,
     password = db_user_password,
+    database = db_name,
+    # `autocommit = True` makes all the subsequent insertion into tables auto commit and doesn't need to call cursor.commit()
+    autocommit = True
 )
+
+# cursor object for executing statements to communicate with MySQL database
+cursor = db.cursor()
+
+@commands.command()
+async def start(context):
+    userID = str(context.author.id)
+    cursor.execute("SELECT EXISTS(SELECT * FROM tb_user WHERE discordID =" + userID + ")")
+    # `SELECT * FROM tb_user WHERE discordID = userID` checks if the user has registered for homelessBeatingBot
+    # `SELECT EXISTS` checks if the search result return anything. if yes, then it returns the number of values return by the searching; otherwise it is 0
+    result = cursor.fetchall()[0][0]
+    # cursor.fetchall() get the results from the last query
+    # in this case the result is: [(1,)] (if the value exist in database)
+    # [(0,)] if the value doesn't exist in database
+    # result is returned in a list, which inside is tuple(s) of values of the results of query 
+    # (thus we use [0][0], the first indexing specify the first tuple, the second indexing specify the first value inside the tuple)
+
+    # create embed to make message pwetty
+    embed = discord.Embed()
+    if (result == 0): # check if the user discord id doesn't exist in database
+        # add the user's discord id into the database
+        cursor.execute("INSERT INTO tb_user (discordID) VALUES(" + userID +")")
+    else:
+        embed.colour = discord.Colour.from_rgb(220, 20, 60) # crimson color cause it is hot
+        # title and description to tell the player they have already registered
+        embed.title = "**__Did you forgor?__**"
+        embed.description = "Darling you have already started your adventure!\nHappy beating homeless! ଘ(੭˃ᴗ˂)੭♡\nHere's a cookie :cookie: and a homeless meme, mwah <3"
+        # set up thumbnail (a smol image at the top right corner)
+        thumbnail = discord.File("emoji/hbb_confused.gif")
+        memeID = "meme" + str(random.randint(0, memeQuantity - 1)) + ".jpg"
+        memeFile = discord.File("homelessMeme/" + memeID)
+        embed.set_thumbnail(url = "attachment://hbb_confused.gif")
+        embed.set_image(url = "attachment://" + memeID)
+
+        await context.message.reply("You have started the adventure silly!", files = [thumbnail, memeFile], embed = embed)
+        # use `file` for single attachment; `files` for a sequence of attachments
+        # source: https://discordpy.readthedocs.io/en/stable/faq.html#how-do-i-upload-an-image
+
+async def setup(bot):
+    bot.add_command(start)
